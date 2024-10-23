899837e99ce44dc8c29fe659a47dda9501a8b10b
codewus <codewus@gmail.com>
2024-10-23 23:53:57 +0900

    [실습예제] 백업

---
+++ react-for-beginners/src/App_todo.js
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault()
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input value={toDo} onChange={onChange} type='text' placeholder='Write your to do...'/>
+        <button>Add To Do</button>
+      </form>
+      <ul>
+        {toDos.map((item, index) => <li key={index}>{item}</li>)}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
