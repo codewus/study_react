855e00cb1d9c43cea8b26d1aaba9d53d7d790a8e
codewus <codewus@gmail.com>
2024-10-27 11:47:30 +0900

    [실습] 배포 시 라우터 오류 설정(절대경로)

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -4,11 +4,11 @@ import Home from './routes/Home';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path={`${process.env.PUBLIC_URL}/hello`} element={<h1>Hello</h1>} />
-        <Route path={`${process.env.PUBLIC_URL}/movie/:id`} element={<Detail></Detail>} />
-        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home></Home>} />
+        <Route path="/hello" element={<h1>Hello</h1>} />
+        <Route path="/movie/:id" element={<Detail></Detail>} />
+        <Route path="/" element={<Home></Home>} />
       </Routes>
     </Router>
   );
